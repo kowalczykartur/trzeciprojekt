c8237af8f0fd7f9828d7ef10f4b495c0f2d73891
kowalczykartur <kowalczyk_artur@interia.eu>
2017-10-29 17:24:43 +0100

    exercise triangle area

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt("Podaj a", "");
+    h = prompt("Podaj h", "");
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
